dee36d93f86d22fa2ccdc112080721a558fc2a78
Joselyn Huaman <j.devetrieva@gmail.com>
2025-03-11 19:46:24 -0400

    creacion_de_icons_portfolio

--- portfolio-joselyn/src/Components/Portada/Portada.jsx
+++ portfolio-joselyn/src/Components/Portada/Portada.jsx
@@ -1,21 +1,27 @@
 import './Portada.scss';
+import GithubPhoto from "../../assets/icon-github.png";
+import LinkedInPhoto from "../../assets/icon-linkedin.png"
 
 
 function Portada(){
 
     return(
 
-        
+        <div>
         <div className='Portada_saludo'>
             <h1>Hi there ! I'm Josy</h1>
         </div>
-        <div>
-            <a href=""></a>
-            <a href=""></a>
-            <a href=""></a>
-            <a href=""></a>
+        <div className='Portada_icons'>
+            <a className='Portada_icons_linkedin' target="_blank" href="https://www.linkedin.com/in/joselyn-judith/">
+                <img src={LinkedInPhoto}/>
+            </a>
+            <a className='Portada_icons_github' target="_blank" href="https://github.com/Josy30/">
+                <img src={GithubPhoto}/>
+            </a>
+            
+            
         </div>
-
+        </div>
 
         
     )
