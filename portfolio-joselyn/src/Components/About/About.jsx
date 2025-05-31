125faf7feb27a8bbd610297cb1122b0ae9b55c7d
RusssianMusketeer <a.devetriev@gmail.com>
2025-05-30 20:44:37 -0400

    background-image

--- portfolio-joselyn/src/Components/About/About.jsx
+++ portfolio-joselyn/src/Components/About/About.jsx
@@ -7,12 +7,12 @@ function About(){
 
     return(
 
-        <div className='About_background'>
-            <img src={backgroundAbout} ></img>
+        <div id="about" className='About_component' >
+            
         <div  className='About_titulo'>
             <h1>ABOUT</h1>
         </div>
-        <div id="about" className='About_component'>
+        <div className='About_container'>
             <div className='About_component_texto'>
                 <h2>A LITTLE BIT OF HISTORY</h2>
                 <span>Since I can remember, I have always been fascinated by art and technology. <br/>I was deeply interested in sketching, painting, crafts, and video games. <br/>I consider myself fortunate to have closely witnessed the technological transformation from indestructible Nokia devices to incredibly powerful modern smartphones.During my high school days, I delved into rooting and custom Android ROMs, and I still remain a proud member of Team Android.</span>
@@ -21,9 +21,9 @@ function About(){
                 <img></img>
             </div>
 
-
+            </div>
         </div>
-        </div>
+        
 
         
     )
