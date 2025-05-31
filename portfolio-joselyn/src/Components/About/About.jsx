3be49934a3223f65a23fee69187e9b8b584f258d
Joselyn Huaman <j.devetrieva@gmail.com>
2025-05-30 20:41:36 -0500

    actualizacion-aboutimg

--- portfolio-joselyn/src/Components/About/About.jsx
+++ portfolio-joselyn/src/Components/About/About.jsx
@@ -1,5 +1,5 @@
 import './About.scss';
-import backgroundAbout from '../../assets/background-about.jpeg'
+import aboutImg from '../../assets/aboutImg.jpeg'
 
 
 
@@ -14,11 +14,10 @@ function About(){
         </div>
         <div className='About_container'>
             <div className='About_component_texto'>
-                <h2>A LITTLE BIT OF HISTORY</h2>
                 <span>Since I can remember, I have always been fascinated by art and technology. <br/>I was deeply interested in sketching, painting, crafts, and video games. <br/>I consider myself fortunate to have closely witnessed the technological transformation from indestructible Nokia devices to incredibly powerful modern smartphones.During my high school days, I delved into rooting and custom Android ROMs, and I still remain a proud member of Team Android.</span>
             </div>
             <div className='About_component_img'>
-                <img></img>
+                
             </div>
 
             </div>
